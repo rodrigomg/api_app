9218e12bb56e1457299942f11dcaeff0abb2deaa
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-08-31 11:10:04 -0500

    coding #1 se configura postgres db

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :api_app, ApiApp.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "root",
   database: "api_app_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
