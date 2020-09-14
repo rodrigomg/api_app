e8dd71ad9a26214375398340a644543a5540ef87
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-09-14 12:01:04 -0500

    coding #1 modificando ruta 'hello' para recibir un parámetro name en la url

--- lib/api_app_web/router.ex
+++ lib/api_app_web/router.ex
@@ -16,7 +16,7 @@ defmodule ApiAppWeb.Router do
   scope "/", ApiAppWeb do
     pipe_through :browser
 
-    get "/hello", HelloController, :world
+    get "/hello/:name", HelloController, :world
     get "/", PageController, :index
   end
 
