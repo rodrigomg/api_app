605ee3c4e1cc46668a681e7ca81728da3f669fba
Jose Cruz <jf.cruz001@ebc.edu.mx>
2020-09-14 12:01:42 -0500

    coding #1 modificando accion world para recibir un mapa de parámetros que contiene el nombre y psarlo al template

--- lib/api_app_web/controllers/hello_controller.ex
+++ lib/api_app_web/controllers/hello_controller.ex
@@ -1,8 +1,8 @@
 defmodule ApiAppWeb.HelloController do
   use ApiAppWeb, :controller
 
-  def world(conn, _params) do
-    render(conn, "world.html")
+  def world(conn, %{"name" => name}) do
+    render(conn, "world.html", name: name)
   end
 
 end
